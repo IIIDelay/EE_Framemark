e12687b76ea29a33de65065779700e1e9bb67a86
IIIDev <1206775631@qq.com>
2023-03-27 18:01:39 +0800

    提交信息: For JD零售的异步工具类

---
+++ async-helper/src/main/java/org/async/worker/ResultState.java
@@ -0,0 +1,12 @@
+package org.async.worker;
+
+/**
+ * 结果状态
+ * @author wuweifeng wrote on 2019-11-19.
+ */
+public enum ResultState {
+    SUCCESS,
+    TIMEOUT,
+    EXCEPTION,
+    DEFAULT  //默认状态
+}
