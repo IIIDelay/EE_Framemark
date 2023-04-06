c76e80b1d275b6c4abb4f618045a7c4b1dd00f3a
IIIDev <1206775631@qq.com>
2023-04-06 22:09:46 +0800

    提交信息: Springboot2.6.x 模块容器验证

--- Springboot2.6.x/src/main/java/org/boot/ApplicationBoot.java
+++ Springboot2.6.x/src/main/java/org/boot/ApplicationBoot.java
@@ -2,6 +2,11 @@ package org.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
 
 /**
  * ApplicationBoot
@@ -11,7 +16,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class ApplicationBoot {
-    public static void main(String[] args) {
-        SpringApplication.run(ApplicationBoot.class, args);
+    public static void main(String[] args) throws IOException {
+        ConfigurableApplicationContext context = SpringApplication.run(ApplicationBoot.class, args);
+        // String message = context.getMessage("test", null, new Locale("en"));
+        Resource resources = context.getResource("file:org/boot/config/DefaultBeanInitiating.java");
+
+        MutablePropertySources propertySources = context.getEnvironment().getPropertySources();
+
+        System.out.println("propertySources = " + propertySources);
+        System.out.println("propertySources.get(\"ResourcePropertySource\") = " + propertySources.get(
+                "class path resource [message.properties]").getProperty("aa"));
+
+        System.out.println("resources.getFile().getName() = " + resources.getFile().getName());
     }
 }
--- Springboot2.6.x/src/main/java/org/boot/config/DefaultBeanInitiating.java
+++ Springboot2.6.x/src/main/java/org/boot/config/DefaultBeanInitiating.java
@@ -1,6 +1,10 @@
 package org.boot.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 /**
  * DefaultBeanInitiating
@@ -9,6 +13,15 @@ import org.springframework.context.annotation.Configuration;
  * @createTime 2023年03月23日 00:22:00
  */
 @Configuration
+@PropertySource("classpath:message.properties")
 public class DefaultBeanInitiating {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("message");
+        return messageSource;
+    }
+
 
 }
